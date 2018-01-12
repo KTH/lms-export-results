b38c76fda7d99477d7edecb980f544756185887b
Emil Stenberg <emilsten@kth.se>
2018-01-12 08:58:23 +0100

    setting firefox profile?

--- test/integration/specs/index.js
+++ test/integration/specs/index.js
@@ -1,26 +1,37 @@
 const test = require('tape')
 require('dotenv').config({path: 'test/.env'})
 
-var webdriverio = require('webdriverio')
-var options = {
-  desiredCapabilities: {
-    browserName: 'firefox'
-  }
-}
+var webdriver = require('selenium-webdriver'),
+  By = webdriver.By,
+  until = webdriver.until
+
+/*****************/
+const firefox = require('selenium-webdriver/firefox')
+
+let profile = new firefox.Profile()
+// profile.addExtension('/path/to/firebug.xpi')
+profile.setPreference("browser.download.folderList",2);
+profile.setPreference("browser.download.manager.showWhenStarting",false);
+// profile.setPreference("browser.download.dir", "/save/file/to/this/directory");
+profile.setPreference("browser.helperApps.neverAsk.saveToDisk","text/csv");
+
+let options = new firefox.Options().setProfile(profile)
+
+/***********/
+
+const driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .setFirefoxOptions(options)
+    .build()
 
 test(`should write a file
     with personnummer and name for the student
     if there's one assignment with one submission in the course`, async t => {
-  webdriverio
-          .remote(options)
-          .init()
-          .url('https://kth.test.instructure.com/login/canvas')
-          .setValue('#pseudonym_session_unique_id',process.env.CANVAS_TESTUSER_USERNAME)
-          .setValue('#pseudonym_session_password',process.env.CANVAS_TESTUSER_PASSWORD)
-          .end()
-          .catch(function (err) {
-            console.log(err)
-          })
+  driver.get('http://www.google.com/ncr')
+  // driver.findElement(By.name('q')).sendKeys('webdriver')
+  // driver.findElement(By.name('btnG')).click()
+  // driver.wait(until.titleIs('webdriver - Google Search'), 1000)
+  driver.quit()
 
   t.end()
 })
