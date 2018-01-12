3465b937961bc694e71ddaa5edfb6ed9b6d4a976
Emil Stenberg <emilsten@kth.se>
2018-01-12 15:09:47 +0100

    downloading the file and waiting for test to timeout

--- test/integration/specs/index.js
+++ test/integration/specs/index.js
@@ -5,21 +5,20 @@ const webdriver = require('selenium-webdriver')
 const By = webdriver.By
 const until = webdriver.until
 
-
 /*****************/
 const firefox = require('selenium-webdriver/firefox')
-
-console.log('eeeeee',firefox.pageLoadStrategy)
 
 let profile = new firefox.Profile()
 // profile.addExtension('/path/to/firebug.xpi')
 profile.setPreference('browser.download.folderList', 2)
-profile.setPreference("browser.download.dir", "/tmp/lms-export-results");
+profile.setPreference('browser.download.dir', '/tmp/lms-export-results')
 profile.setPreference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
+profile.setPreference("webdriver.load.strategy", "unstable");
 
-debugger
+console.log('todo: remove the /tmp/lms-export-results directory!')
+console.log('Todo: set the page timeout to something less.')
 let options = new firefox.Options().setProfile(profile)
-
+const fs = require('fs')
 /***********/
 
 const driver = new webdriver.Builder()
@@ -30,24 +29,28 @@ const driver = new webdriver.Builder()
 test(`should write a file
     with personnummer and name for the student
     if there's one assignment with one submission in the course`, async t => {
-  driver.get('https://kth.test.instructure.com/login/canvas')
+  await driver.get('https://kth.test.instructure.com/login/canvas')
 
-  driver.findElement(By.id('pseudonym_session_unique_id')).sendKeys(process.env.CANVAS_TESTUSER_USERNAME)
-  driver.findElement(By.id('pseudonym_session_password')).sendKeys(process.env.CANVAS_TESTUSER_PASSWORD)
-  driver.findElement(By.className('Button--login')).click()
-  driver.get('https://kth.test.instructure.com/courses/4/external_tools/536?display=borderless')
-  try{
-      driver.findElement(By.css('input[type="submit"]')).click()
-  }catch(e){
-    console.log('timeout...')
+  await driver.findElement(By.id('pseudonym_session_unique_id')).sendKeys(process.env.CANVAS_TESTUSER_USERNAME)
+  await driver.findElement(By.id('pseudonym_session_password')).sendKeys(process.env.CANVAS_TESTUSER_PASSWORD)
+  await driver.findElement(By.className('Button--login')).click()
+  await driver.get('https://kth.test.instructure.com/courses/4/external_tools/536?display=borderless')
+  try {
+    // driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS)
+    await driver.findElement(By.css('input[type="submit"]')).click()
+  } catch (e) {
+    console.log('timeout. This is good, now check file content...', )
   }
-  console.log('todo: check the downloaded file!')
+
+  fs.readdirSync('/tmp/lms-export-results').forEach(file => {
+    console.log(file)
+  })
   // driver.wait(until.titleIs('webdriver - Google Search'), 1000)
 
   // driver.findElement(By.name('q')).sendKeys('webdriver')
   // driver.findElement(By.name('btnG')).click()
   // driver.wait(until.titleIs('webdriver - Google Search'), 1000)
-  driver.quit()
+  await driver.quit()
 
   t.end()
 })
