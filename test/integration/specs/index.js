b4c00ba6bbc6a10878af8c36ee179d48cd0ddd84
Emil Stenberg <emilsten@kth.se>
2018-01-12 14:05:53 +0100

    downloading file without dialog

--- test/integration/specs/index.js
+++ test/integration/specs/index.js
@@ -1,47 +1,46 @@
 const test = require('tape')
 require('dotenv').config({path: 'test/.env'})
-// http://webdriver.io/guide/services/firefox-profile.html
-const webdriverio = require('webdriverio')
-const FirefoxProfile = require('firefox-profile')
+
+const webdriver = require('selenium-webdriver')
+const By = webdriver.By
+const until = webdriver.until
 
 
-// Creating a profile. But for some reason this is not used.
-const fp = new FirefoxProfile()
-fp.setPreference('browser.download.folderList', 1)
-fp.setPreference('browser.download.manager.showWhenStarting', true)
-fp.setPreference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
-fp.setPreference('browser.newtab.url', 'http://saadtazi.com')
-fp.updatePreferences()
+/*****************/
+const firefox = require('selenium-webdriver/firefox')
+
+let profile = new firefox.Profile()
+// profile.addExtension('/path/to/firebug.xpi')
+profile.setPreference('browser.download.folderList', 2)
+profile.setPreference('browser.download.manager.showWhenStarting', false)
+// profile.setPreference("browser.download.dir", "/save/file/to/this/directory");
+profile.setPreference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
+
+let options = new firefox.Options().setProfile(profile)
+
+/***********/
+
+const driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .setFirefoxOptions(options)
+    .build()
 
 test(`should write a file
     with personnummer and name for the student
     if there's one assignment with one submission in the course`, async t => {
-  fp.encoded((err, profile) =>{
-    var options = {
-      desiredCapabilities: {
-        // pageLoadStrategy: 'eager',
-        browserName: 'firefox',
-        firefox_profile: profile
-      }
-    }
+  driver.get('https://kth.test.instructure.com/login/canvas')
 
-    webdriverio
-            .remote(options)
-            .init()
-            .url('https://kth.test.instructure.com/login/canvas')
-            .setValue('#pseudonym_session_unique_id', process.env.CANVAS_TESTUSER_USERNAME)
-            .setValue('#pseudonym_session_password', process.env.CANVAS_TESTUSER_PASSWORD)
-            .click('#login_form .Button--login')
-            .url('https://kth.test.instructure.com/courses/4/external_tools/536?display=borderless')
-            .click('input[type="submit"]')
-            .then(() => {
-              t.end()
-            })
-            .end()
-            .catch(function (err) {
-              console.log(err)
-            })
-  })
+  driver.findElement(By.id('pseudonym_session_unique_id')).sendKeys(process.env.CANVAS_TESTUSER_USERNAME)
+  driver.findElement(By.id('pseudonym_session_password')).sendKeys(process.env.CANVAS_TESTUSER_PASSWORD)
+  driver.findElement(By.className('Button--login')).click()
+  driver.get('https://kth.test.instructure.com/courses/4/external_tools/536?display=borderless')
+  driver.findElement(By.css('input[type="submit"]')).click()
+  // driver.wait(until.titleIs('webdriver - Google Search'), 1000)
+
+  // driver.findElement(By.name('q')).sendKeys('webdriver')
+  // driver.findElement(By.name('btnG')).click()
+  // driver.wait(until.titleIs('webdriver - Google Search'), 1000)
+  driver.quit()
 
   t.end()
 })
