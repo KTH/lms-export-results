ca7faac43a6695551caeb7a805c8923677012012
Emil Stenberg <emilsten@kth.se>
2018-01-12 09:12:11 +0100

    login with webdriver

--- test/integration/specs/index.js
+++ test/integration/specs/index.js
@@ -1,19 +1,20 @@
 const test = require('tape')
 require('dotenv').config({path: 'test/.env'})
 
-var webdriver = require('selenium-webdriver'),
-  By = webdriver.By,
-  until = webdriver.until
+const webdriver = require('selenium-webdriver')
+const By = webdriver.By
+const until = webdriver.until
+
 
 /*****************/
 const firefox = require('selenium-webdriver/firefox')
 
 let profile = new firefox.Profile()
 // profile.addExtension('/path/to/firebug.xpi')
-profile.setPreference("browser.download.folderList",2);
-profile.setPreference("browser.download.manager.showWhenStarting",false);
+profile.setPreference('browser.download.folderList', 2)
+profile.setPreference('browser.download.manager.showWhenStarting', false)
 // profile.setPreference("browser.download.dir", "/save/file/to/this/directory");
-profile.setPreference("browser.helperApps.neverAsk.saveToDisk","text/csv");
+profile.setPreference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
 
 let options = new firefox.Options().setProfile(profile)
 
@@ -27,7 +28,13 @@ const driver = new webdriver.Builder()
 test(`should write a file
     with personnummer and name for the student
     if there's one assignment with one submission in the course`, async t => {
-  driver.get('http://www.google.com/ncr')
+  driver.get('https://kth.test.instructure.com/login/canvas')
+
+  driver.findElement(By.id('pseudonym_session_unique_id')).sendKeys(process.env.CANVAS_TESTUSER_USERNAME)
+  driver.findElement(By.id('pseudonym_session_password')).sendKeys(process.env.CANVAS_TESTUSER_PASSWORD)
+  driver.findElement(By.className('Button--login')).click()
+  // driver.wait(until.titleIs('webdriver - Google Search'), 1000)
+
   // driver.findElement(By.name('q')).sendKeys('webdriver')
   // driver.findElement(By.name('btnG')).click()
   // driver.wait(until.titleIs('webdriver - Google Search'), 1000)
