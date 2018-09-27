82398ea7b4878c498fc3cbbd4207151858cbcc92
Carlos Saito <c.saito.style@gmail.com>
2018-09-27 15:44:56 +0200

    Rename test file

--- test/specs/resultsFile.getHeaders.test.js
+++ test/specs/resultsFile.getHeaders.test.js
@@ -133,3 +133,56 @@ test('"getHeaders()" returns 15 headers when data are four assignment (7 fixed +
 
   t.end()
 })
+
+test('"getHeaders() returns 10 headers when data are 1 assignment + 1 custom column (7 fixed + 2 per assignemnt + 1 per custom column', async t => {
+  class FakeCanvasApi {
+    get (url) {
+      if (url.includes('assignments')) {
+        return [{id: 'a1', name: 'a1name'}]
+      } else if (url.includes('custom_gradebook_columns') && !url.includes('data')) {
+        return [{position: 0, title: 'custom column 1'}]
+      } else {
+        return []
+      }
+    }
+  }
+
+  ResultsFile.__set__('CanvasApi', FakeCanvasApi)
+  const file = await getInstance()
+
+  await file.preload()
+
+  const headers = file.getHeaders()
+
+  t.equal(headers.length, 10, 'should return exactly 10 headers')
+  t.equal(headers[7], 'custom column 1', '8th header should be called "custom column 1"')
+  t.end()
+})
+
+test('"getHeaders() returns custom columns sorted by "position" in Canvas', async t => {
+  class FakeCanvasApi {
+    get (url) {
+      if (url.includes('custom_gradebook_columns') && !url.includes('data')) {
+        return [
+          {position: 2, title: 'custom column 2'},
+          {position: 0, title: 'custom column 0'},
+          {position: 1, title: 'custom column 1'}
+        ]
+      } else {
+        return []
+      }
+    }
+  }
+
+  ResultsFile.__set__('CanvasApi', FakeCanvasApi)
+  const file = await getInstance()
+
+  await file.preload()
+
+  const headers = file.getHeaders()
+
+  t.equal(headers[7], 'custom column 0', '8th header should be called "custom column 0"')
+  t.equal(headers[8], 'custom column 1', '9th header should be called "custom column 1"')
+  t.equal(headers[9], 'custom column 2', '10th header should be called "custom column 2"')
+  t.end()
+})
