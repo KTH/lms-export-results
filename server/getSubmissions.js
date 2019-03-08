1bb27ee158a4f28861dad7a80d1dd93c6340ff04
Emil <emilsten@kth.se>
2019-03-08 11:12:50 +0100

    test is failing

--- server/getSubmissions.js
+++ server/getSubmissions.js
@@ -1,8 +1,16 @@
 const CanvasApi = require('kth-canvas-api')
+const assert = require('assert')
+require('dotenv').config()
+
 const test = require('tape')
 const canvasApi = new CanvasApi(process.env.CANVAS_HOST, process.env.CANVAS_TOKEN)
-
-test('', async t =>{
-   t.equal(1,3) 
+test('should construct the similar json', async t =>{
+    const groupedJson = await canvasApi.get(`courses/3719/students/submissions?grouped=1&student_ids[]=all&per_page=100`)
+    const builtJson = await getSubmissions()
+    assert.deepEqual(groupedJson,builtJson) 
+    t.end()
 })
 
+async function getSubmissions() {
+   return {} 
+}
