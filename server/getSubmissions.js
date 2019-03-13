2f5567c12a65167e4dc9cc07f8e0717dfdfe279b
Emil <emilsten@kth.se>
2019-03-13 13:44:08 +0100

    the jsons are now equal in the important parts

--- server/getSubmissions.js
+++ server/getSubmissions.js
@@ -6,13 +6,15 @@ require('dotenv').config()
 const test = require('tape')
 const canvasApi = new CanvasApi(process.env.CANVAS_HOST, process.env.CANVAS_TOKEN)
 test('should construct the similar json', async t =>{
-    const groupedJson = await canvasApi.get(`courses/3719/students/submissions?grouped=1&student_ids[]=all&per_page=100`)
+    const groupedJson = (await canvasApi.get(`courses/3719/students/submissions?grouped=1&student_ids[]=all&per_page=100`)).sort((a,b)=> a.user_id - b.user_id).filter(stud => stud.user_id !== 55842)
     //console.log('===============================') 
     //console.log(JSON.stringify(groupedJson))
     //console.log('-----------------------------')
     const groupedStudent = groupedJson.find(gj => gj.user_id === 51429)
+    
+    // ignore the test student
+    const builtJson = (await getSubmissions()).sort((a,b)=> a.user_id - b.user_id)
 
-    const builtJson = await getSubmissions()
     const builtStudent= builtJson.find(gj => gj.user_id === 51429)
 
 
@@ -28,12 +30,12 @@ test('should construct the similar json', async t =>{
     groupedJson.forEach(student => student.submissions.forEach(sub => delete sub.seconds_late))
     builtJson.forEach(student => student.submissions.forEach(sub => delete sub.seconds_late))
 
-    console.log('******************************')
-
+//console.log(jsonDiff.diffString({ foo: 'bar' }, { foo: 'baz' }));
+     //console.log(jsonDiff.diffString(builtJson.map(s => s.user_id), groupedJson.map(s => s.user_id)))
+    console.log(groupedJson.map(s => s.user_id))
 
     assert.deepEqual(builtJson.length, groupedJson.length)
-    assert.deepEqual(builtStudent, groupedStudent)
-//    assert.deepEqual(groupedJson,builtJson) 
+    assert.deepEqual(groupedJson,builtJson) 
 
     t.end()
 })
@@ -54,7 +56,11 @@ async function getSubmissions() {
     const students = [].concat(...studentsPerSection)
     const submissions = await canvasApi.get('courses/3719/students/submissions?student_ids[]=all&per_page=100')
     const result = students.map( student => ({...student, submissions:submissions.filter(sub => sub.user_id === student.user_id)}) )
+    
+    //
+    const groupedJson = await canvasApi.get(`courses/3719/students/submissions?grouped=1&student_ids[]=all&per_page=100`)
 
+    console.log(students.length,result.length, groupedJson.length)
     return result 
 
 
