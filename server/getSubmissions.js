e47105d8c87481123838917a44a57857acfe3304
Emil <emilsten@kth.se>
2019-03-13 13:59:07 +0100

    refactor

--- server/getSubmissions.js
+++ server/getSubmissions.js
@@ -1,50 +1,35 @@
 const CanvasApi = require('kth-canvas-api')
-const jsonDiff = require('json-diff')
+
+// TODO: delete these test imports
 const assert = require('assert')
+const test = require('tape')
+
+
 require('dotenv').config()
 
-const test = require('tape')
 const canvasApi = new CanvasApi(process.env.CANVAS_HOST, process.env.CANVAS_TOKEN)
+
+
+// TODO: This function should be deleted before we merge the PR. I think this test has done its good once the code is merged.
 test('should construct the similar json', async t =>{
-    const groupedJson = (await canvasApi.get(`courses/3719/students/submissions?grouped=1&student_ids[]=all&per_page=100`)).sort((a,b)=> a.user_id - b.user_id).filter(stud => stud.user_id !== 55842)
-    //console.log('===============================') 
-    //console.log(JSON.stringify(groupedJson))
-    //console.log('-----------------------------')
-    const groupedStudent = groupedJson.find(gj => gj.user_id === 51429)
+    // Get the json, sorted, without the test user. The new function don't incluce the test users.
+    const oldWay= (await canvasApi.get(`courses/3719/students/submissions?grouped=1&student_ids[]=all&per_page=100`)).sort((a,b)=> a.user_id - b.user_id).filter(stud => stud.user_id !== 55842)
     
-    // ignore the test student
-    const builtJson = (await getSubmissions()).sort((a,b)=> a.user_id - b.user_id)
+    // Make sure both arrays are sorted, so I can compare them
+    const newWay = (await getSubmissions(3719)).sort((a,b)=> a.user_id - b.user_id)
 
-    const builtStudent= builtJson.find(gj => gj.user_id === 51429)
+    // Ignore seconds late when comparing, since these are changed for every api call
+    oldWay.forEach(student => student.submissions.forEach(sub => delete sub.seconds_late))
+    newWay.forEach(student => student.submissions.forEach(sub => delete sub.seconds_late))
 
-
-    console.log('###############################') 
-    console.log(JSON.stringify(groupedJson.find(stud => stud.user_id === 774)))
-    console.log('-------------------------------')
-    console.log(JSON.stringify(builtJson.find(stud => stud.user_id === 774)))
-
-    // Ignore seconds late when comparing
-    builtStudent.submissions.forEach(sub => delete sub.seconds_late)
-    groupedStudent.submissions.forEach(sub => delete sub.seconds_late)
-
-    groupedJson.forEach(student => student.submissions.forEach(sub => delete sub.seconds_late))
-    builtJson.forEach(student => student.submissions.forEach(sub => delete sub.seconds_late))
-
-//console.log(jsonDiff.diffString({ foo: 'bar' }, { foo: 'baz' }));
-     //console.log(jsonDiff.diffString(builtJson.map(s => s.user_id), groupedJson.map(s => s.user_id)))
-    console.log(groupedJson.map(s => s.user_id))
-
-    assert.deepEqual(builtJson.length, groupedJson.length)
-    assert.deepEqual(groupedJson,builtJson) 
+    assert.deepEqual(oldWay,newWay) 
 
     t.end()
 })
 
-async function getSubmissions() {
+async function getSubmissions(courseId) {
 
-    const sections = await canvasApi.get('courses/3719/sections?include[]=students')
-
-    // TODO: should flatten the array, so students is only a flat map. Reduce?
+    const sections = await canvasApi.get(`courses/${courseId}/sections?include[]=students`)
 
     const studentsPerSection = sections.map(section => section.students.map(student => ({
         user_id:student.id, 
@@ -53,16 +38,12 @@ async function getSubmissions() {
         sis_user_id: student.sis_user_id,
         integration_id: student.integration_id
     })))
+    
+    // Flatten the students array
     const students = [].concat(...studentsPerSection)
-    const submissions = await canvasApi.get('courses/3719/students/submissions?student_ids[]=all&per_page=100')
+    const submissions = await canvasApi.get(`courses/${courseId}/students/submissions?student_ids[]=all&per_page=100`)
     const result = students.map( student => ({...student, submissions:submissions.filter(sub => sub.user_id === student.user_id)}) )
     
-    //
-    const groupedJson = await canvasApi.get(`courses/3719/students/submissions?grouped=1&student_ids[]=all&per_page=100`)
-
-    console.log(students.length,result.length, groupedJson.length)
     return result 
-
-
-
 }
+module.exports = getSubmissions
