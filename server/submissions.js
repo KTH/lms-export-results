3cac455a44b07bc6d1020edab3ff2ee4f46d1943
Peter Delmeby <peter.delmeby@gmail.com>
2019-04-24 14:27:47 +0200

    Clarified special link header handling.

--- server/submissions.js
+++ server/submissions.js
@@ -1,3 +1,18 @@
+// Note: The purpose of this function is to extract the query string from the Link header,
+// for the so called "first" link.
+function getFirstLinkQSParams (headers) {
+  let firstQSParams = {}
+  const first = headers.link.split(',').find(l => l.search(/rel="first"$/) !== -1)
+  const matchedUrl = first && first.match(/<(.*?)>/)
+  const splitUrl = matchedUrl[1].split('?')
+  const qsArray = splitUrl[1].split('&')
+  for (const item of qsArray) {
+    const splitItem = item.split('=')
+    firstQSParams[splitItem[0]] = splitItem[1]
+  }
+  return firstQSParams
+}
+
 async function getSubmissions ({ canvasCourseId, sections, canvasApi }) {
   const studentsPerSection = sections.map(section => section.students.map(student => ({
     user_id: student.id,
@@ -11,24 +26,14 @@ async function getSubmissions ({ canvasCourseId, sections, canvasApi }) {
   const students = [].concat(...studentsPerSection)
   const url = `/courses/${canvasCourseId}/students/submissions`
   const response = await canvasApi.get(url, { student_ids: 'all' })
-  const next = response.headers.link.split(',').find(l => l.search(/rel="first"$/) !== -1)
-  const nextUrl = next && next.match(/<(.*?)>/)
-  if (nextUrl && nextUrl[1]) {
-    const splitUrl = nextUrl[1].split('?')
-    const qsArray = splitUrl[1].split('&')
-    const qsParams = {}
-    for (const item of qsArray) {
-      const splitItem = item.split('=')
-      qsParams[splitItem[0]] = splitItem[1]
-    }
-    const submissions = []
-    for await (const submission of canvasApi.list(url, qsParams)) {
-      submissions.push(submission)
-    }
-
-    return students.map(student => ({ ...student, submissions: submissions.filter(sub => sub.user_id === student.user_id) }))
-  } else {
-    throw Error('Get submissions failed due to missing Link header')
+  // Note: Speculative special handling of pagination when fetching submissions.
+  // Want to use the query string to specifically fetch the "first" page.
+  const qsParams = getFirstLinkQSParams(response.headers)
+  const submissions = []
+  for await (const submission of canvasApi.list(url, qsParams)) {
+    submissions.push(submission)
   }
+
+  return students.map(student => ({ ...student, submissions: submissions.filter(sub => sub.user_id === student.user_id) }))
 }
 module.exports = { getSubmissions }
