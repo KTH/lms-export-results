02a5fe4d677a385b87445023dad695485eafa3dc
Peter Delmeby <peter.delmeby@gmail.com>
2019-05-02 13:58:11 +0200

    Reducing timeout quite a bit.

--- server/export.js
+++ server/export.js
@@ -219,7 +219,7 @@ function getCustomColumnHeaders (customColumns) {
 }
 
 async function exportResults3 (req, res) {
-  req.setTimeout && req.setTimeout(25 * 60 * 1000)
+  req.setTimeout && req.setTimeout(10 * 60 * 1000)
   const correlationId = req.query.correlationId || req.id
   const courseRound = req.query.courseRound
   const fileName = `${courseRound || 'canvas'}-${moment().format('YYYYMMDD-HHMMSS')}-results.csv`
