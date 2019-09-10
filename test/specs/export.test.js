64e16ad2463b1f1ea313be802d25080652dd6cdf
Peter Delmeby <peter.delmeby@gmail.com>
2019-09-10 11:24:22 +0200

    Mocked isAllowed in functional test.

--- test/specs/export.test.js
+++ test/specs/export.test.js
@@ -61,6 +61,7 @@ test(`should write a file
   const req = { query: { courseRound: 'round', canvasCourseId: 'canvasCourseId' }, get: () => '' }
 
   _export.__set__('getAccessToken', () => 'mocked token')
+  _export.__set__('isAllowed', () => true)
   await exportResults3(req, res)
   sinon.assert.calledWith(res.write, '\uFEFF')
   sinon.assert.calledWith(res.write, sinon.match('SIS User ID;ID;Section;Name;Surname;Personnummer'))
