5998eecccf1c199735f6fa2343697f16113c107e
Emil Stenberg <esandin@gmail.com>
2021-10-27 14:36:50 +0200

    format

--- server/systemroutes.js
+++ server/systemroutes.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const packageFile = require('../package.json')
-const defaultLog = require('./log')
-const ldap = require('./ldap')
-const version = require('../config/version')
+const express = require("express");
+const packageFile = require("../package.json");
+const defaultLog = require("./log");
+const ldap = require("./ldap");
+const version = require("../config/version");
 
-const router = express.Router()
+const router = express.Router();
 
-function _about (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
+function _about(req, res) {
+  res.setHeader("Content-Type", "text/plain");
   res.send(`
     packageFile.name:${packageFile.name}
     packageFile.version:${packageFile.version}
@@ -18,17 +18,17 @@ function _about (req, res) {
     version.dockerName:${version.dockerName}
     version.dockerVersion:${version.dockerVersion}
     version.jenkinsBuildDate:${version.jenkinsBuildDate}
-  `)
+  `);
 }
 
-async function _monitor (req, res) {
-  const log = req.log || defaultLog
+async function _monitor(req, res) {
+  const log = req.log || defaultLog;
 
-  res.setHeader('Content-Type', 'text/plain')
-  res.send('APPLICATION_STATUS: OK')
+  res.setHeader("Content-Type", "text/plain");
+  res.send("APPLICATION_STATUS: OK");
 }
 
-router.get('/_monitor', _monitor)
-router.get('/_about', _about)
+router.get("/_monitor", _monitor);
+router.get("/_about", _about);
 
-module.exports = router
+module.exports = router;
