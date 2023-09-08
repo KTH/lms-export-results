09940b536ac7163466d2cf0008f2e9f490cbb3bd
Carlos Saito <carlos@saito.style>
2023-09-08 11:20:26 +0200

    Get personal number information from Ladok

---
+++ server/ladokApi.js
@@ -0,0 +1,109 @@
+/* eslint-disable no-await-in-loop */
+const got = require("got");
+
+const gotClient = got.extend({
+  prefixUrl: process.env.LADOK_API_BASEURL,
+  headers: {
+    Accept: "application/vnd.ladok-resultat+json",
+  },
+  responseType: "json",
+  https: {
+    pfx: Buffer.from(process.env.LADOK_API_PFX_BASE64, "base64"),
+    passphrase: process.env.LADOK_API_PFX_PASSPHRASE,
+  },
+});
+
+/**
+ * Get the structure of a course round (kurstillfälle)
+ * @see {@link https://www.integrationstest.ladok.se/restdoc/resultat.html#h%C3%A4mtaIng%C3%A5endeMomentF%C3%B6rKurstillf%C3%A4lle}
+ */
+function getKurstillfalleStructure(kurstillfalleUID) {
+  return gotClient
+    .get(`resultat/kurstillfalle/${kurstillfalleUID}/moment`)
+    .then((response) => response.body);
+}
+/**
+ * Get all Studieresultat given some filters
+ *
+ * @see {@link https://www.integrationstest.ladok.se/restdoc/resultat.html#s%C3%B6kStudieresultatF%C3%B6rRapporteringMedRequestbody} for documentation
+ * when type is "utbildningsinstans" and
+ * @see {@link https://www.integrationstest.ladok.se/restdoc/resultat.html#s%C3%B6kStudieresultatAttRapporteraP%C3%A5Aktivitetstillf%C3%A4lleMedRequestbody} for documentation
+ * when type is "aktivitetstillfalle"
+ */
+function searchStudieresultat(UID = "", KurstillfallenUID = [], page = 1) {
+  return gotClient
+    .put(`resultat/studieresultat/rapportera/utbildningsinstans/${UID}/sok`, {
+      json: {
+        Filtrering: ["OBEHANDLADE", "UTKAST", "KLARMARKERADE", "ATTESTERADE"],
+        KurstillfallenUID,
+        // NOTE: OrderBy MUST be included always.
+        // Otherwise the pagination will be broken because Ladok does not sort
+        // things consistently by default
+        OrderBy: ["PERSONNUMMER_ASC"],
+        Page: page,
+      },
+    })
+    .then((r) => r.body);
+}
+
+async function _searchAllStudieresultat(UID, KurstillfallenUID) {
+  let page = 1;
+  const allResults = [];
+  const result = await searchStudieresultat(UID, KurstillfallenUID, page);
+  allResults.push(...result.Resultat);
+
+  while (result.TotaltAntalPoster > allResults.length) {
+    page++;
+    // eslint-disable-next-line no-await-in-loop, no-shadow
+    const result = await searchStudieresultat(UID, KurstillfallenUID, page);
+    allResults.push(...result.Resultat);
+  }
+
+  return allResults;
+}
+
+/** Given a list of kurstillfalle UID, get a map of student IDs and their personal number */
+async function getPersonalNumbers(kurstillfalleIds) {
+  // Get all "Utbildningsinstans"
+  const mapWithPersonalNumbers = new Map();
+  const utbildningsinstansUIDs = new Set();
+
+  for (const kurstillfalleId of kurstillfalleIds) {
+    utbildningsinstansUIDs.add(
+      await getKurstillfalleStructure(kurstillfalleId).then(
+        (kurstillfalle) => kurstillfalle.UtbildningsinstansUID
+      )
+    );
+  }
+
+  // For each utbildningsinstans, get all results
+  for (const utbildningsinstansUID of utbildningsinstansUIDs) {
+    const studieresultat = await _searchAllStudieresultat(
+      utbildningsinstansUID,
+      kurstillfalleIds
+    );
+
+    for (const s of studieresultat) {
+      mapWithPersonalNumbers.set(s.Student.Uid, s.Student.Personnummer);
+    }
+  }
+
+  return mapWithPersonalNumbers;
+}
+
+module.exports = class LadokApi {
+  /** Prepares a Ladok Client to get students given a list of course rounds */
+  constructor(kurstillfalleIds) {
+    this.kurstillfalleIds = kurstillfalleIds;
+    this.personalNumbers = null;
+  }
+
+  /** Return the personal number given a Ladok ID of a student */
+  async getPersonalNumber(studentUid) {
+    if (!this.personalNumbers) {
+      this.personalNumbers = await getPersonalNumbers(this.kurstillfalleIds);
+    }
+
+    return this.personalNumbers.get(studentUid);
+  }
+};
