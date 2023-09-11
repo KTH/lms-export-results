3ee1042cb7cdb9a7f1f9a22b3c56114232a7d5a6
Carlos Saito <carlos@saito.style>
2023-09-11 11:51:53 +0200

    Add App Insights

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const log = require("./server/log");
 
 log.info("canvas client id:", process.env.CANVAS_CLIENT_ID);
 
+if (process.env.AZURE_APPINSIGHTS_INSTRUMENTATION_KEY) {
+  const appInsights = require("applicationinsights");
+  appInsights
+    .setup(process.env.AZURE_APPINSIGHTS_INSTRUMENTATION_KEY)
+    .setAutoDependencyCorrelation(false)
+    .setAutoCollectRequests(true)
+    .setAutoCollectPerformance(true)
+    .setAutoCollectExceptions(false)
+    .setAutoCollectDependencies(true)
+    .setAutoCollectConsole(true)
+    .start();
+}
+
 module.exports = server.start({
   useSsl: false,
   port: process.env.SERVER_PORT || process.env.PORT || 3001,
